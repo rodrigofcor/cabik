f6719db921d49c849f8da019ad89dfd8ad4a6966
Rodrigo Fagundes Coruja <rodrigofagundescoruja@gmail.com>
2022-05-01 01:30:03 -0300

    add bootstrap and created template section

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="app">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Criar Perfil')
+
+@section('content')
+
+@endsection
